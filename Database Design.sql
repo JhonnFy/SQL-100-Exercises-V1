165fbdb3cbd1d03ca37a3fd475b5eb928a209a6f
Jhon fy <moesdeveloper1023@gmail.com>
2025-08-24 00:56:13 -0500

    Crear La Tabla Ubicacion

--- Database Design.sql
+++ Database Design.sql
@@ -1,5 +1,12 @@
 
 USE AdminDeveloper
 
---Database Desing
+
+--Id, Nombre, Ubicacion
+--CREATE TABLE UBICACION(
+--	Id_Ubicacion INT IDENTITY(1,1),
+--	Nombre VARCHAR(100) NOT NULL,
+--	Ubicacion VARCHAR(100) NOT NULL,
+--	PRIMARY KEY(Id_Ubicacion)
+--)
 
