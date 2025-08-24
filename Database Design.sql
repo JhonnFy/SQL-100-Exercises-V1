4683e15e81198c9e3370d3454b5db07f1b5cc8d6
Jhon fy <moesdeveloper1023@gmail.com>
2025-08-24 01:18:25 -0500

    Crear La Tabla Proyecto

--- Database Design.sql
+++ Database Design.sql
@@ -17,9 +17,21 @@ USE AdminDeveloper
 --	Nombre VARCHAR(100) NOT NULL,
 --	Apellido VARCHAR(100) NOT NULL,
 --	Email VARCHAR(100) NOT NULL,
---	Salario DECIMAL(1,1) NOT NULL,
---	Fecha_Contratacion DATETIME NOT NULL,
+--	Salario DECIMAL(10,1) NOT NULL,
+--	Fecha_Contratacion DATE NOT NULL,
 --	FK_UbicacionId INT 
 --	PRIMARY KEY (Id_Empleado)
 --)
+
+
+--PROYECTO,Id_Proyecto,Nombre,Fecha_Inicio,Fecha_Fin,Presupuesto
+--CREATE TABLE PROYECTO(
+--	Id_Proyecto INT IDENTITY(1,1),
+--	Nombre VARCHAR(100) NOT NULL,
+--	Fecha_Inicio DATE NOT NULL,
+--	Fecha_Fin DATE NOT NULL,
+--	Presupuesto DECIMAL(10,1),
+--	PRIMARY KEY (Id_Proyecto)
+--)
+
 
