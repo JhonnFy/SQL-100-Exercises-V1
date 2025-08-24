ed1576dd5a7d527d3600541c54f62640164749cb
Jhon fy <moesdeveloper1023@gmail.com>
2025-08-24 01:08:15 -0500

    Crear La Tabla Empleado

--- Database Design.sql
+++ Database Design.sql
@@ -10,3 +10,16 @@ USE AdminDeveloper
 --	PRIMARY KEY(Id_Ubicacion)
 --)
 
+
+--EMPLEADO,Id_Empleado,Nombre,Apellido,Email,Salario,Fecha_Contratacion,FK_UnicacionId
+--CREATE TABLE EMPLEADO(
+--	Id_Empleado INT IDENTITY(1,1),
+--	Nombre VARCHAR(100) NOT NULL,
+--	Apellido VARCHAR(100) NOT NULL,
+--	Email VARCHAR(100) NOT NULL,
+--	Salario DECIMAL(1,1) NOT NULL,
+--	Fecha_Contratacion DATETIME NOT NULL,
+--	FK_UbicacionId INT 
+--	PRIMARY KEY (Id_Empleado)
+--)
+
